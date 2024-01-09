06be1a228b1d00a59fe371881581b43e0110309d
Junnnnnnnnnnn <wjsguswls71@naver.com>
2024-01-09 15:18:09 +0900

    test: jenkins test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Maestro Pitch!';
+    return 'Hello Maestro Pitch! jenkins';
   }
 }
