921d41f09ba5b966ee0bc6f9d36bfede1cf9b047
Junnnnnnnnnnn <wjsguswls71@naver.com>
2024-01-16 15:48:48 +0900

    fix: log ctx format 변경

--- src/log/logProvider.ts
+++ src/log/logProvider.ts
@@ -5,20 +5,24 @@ export class LogProvider {
   constructor() {}
 
   static info(ctx: Record<string, any>, method: string) {
-    console.log({
-      requestId: rTracer.id(),
-      ctx,
-      method,
-      level: LogLevel.INFO,
-    });
+    console.log(
+      JSON.stringify({
+        requestId: rTracer.id(),
+        ctx,
+        method,
+        level: LogLevel.INFO,
+      }),
+    );
   }
 
   static error(ctx: Record<string, any>, method: string): void {
-    console.log({
-      ...(rTracer.id() ? { requestId: rTracer.id() } : {}),
-      method,
-      ctx,
-      level: LogLevel.ERROR,
-    });
+    console.log(
+      JSON.stringify({
+        ...(rTracer.id() ? { requestId: rTracer.id() } : {}),
+        method,
+        ctx,
+        level: LogLevel.ERROR,
+      }),
+    );
   }
 }
