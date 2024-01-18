668d13886736a9e675f878267e5fddca8deb4082
Junnnnnnnnnnn <wjsguswls71@naver.com>
2024-01-18 16:43:18 +0900

    feat: error code message 추가

--- src/exception/enum/error.enum.ts
+++ src/exception/enum/error.enum.ts
@@ -2,12 +2,18 @@ import { HttpStatus } from '@nestjs/common';
 
 export enum ErrorCode {
   UNAUTHORIZED = 'Unauthorized',
+  JWT_UNABLE_VERIFY = 'Jwt unable verify',
+  NOT_FOUND_CONTENT = 'Not found content',
 }
 
 export const ErrorMessage: { [key in ErrorCode]: string } = {
   [ErrorCode.UNAUTHORIZED]: 'Invalid Access Token',
+  [ErrorCode.JWT_UNABLE_VERIFY]: 'Unable verify of jwt',
+  [ErrorCode.NOT_FOUND_CONTENT]: 'Not found content',
 };
 
 export const ErrorStatus: { [key in ErrorCode]: HttpStatus } = {
   [ErrorCode.UNAUTHORIZED]: HttpStatus.UNAUTHORIZED,
+  [ErrorCode.JWT_UNABLE_VERIFY]: HttpStatus.UNAUTHORIZED,
+  [ErrorCode.NOT_FOUND_CONTENT]: HttpStatus.NOT_FOUND,
 };
