80c849885339a838459717cbbd2a69bd298febb6
Junnnnnnnnnnn <wjsguswls71@naver.com>
2024-02-15 15:20:37 +0900

    error enum 추가

--- src/exception/enum/error.enum.ts
+++ src/exception/enum/error.enum.ts
@@ -1,19 +1,25 @@
 import { HttpStatus } from '@nestjs/common';
 
 export enum ErrorCode {
-  UNAUTHORIZED = 'Unauthorized',
-  JWT_UNABLE_VERIFY = 'Jwt unable verify',
-  NOT_FOUND_CONTENT = 'Not found content',
+  UNAUTHORIZED = 'unauthorized',
+  JWT_UNABLE_VERIFY = 'jwt_unable_verify',
+  NOT_FOUND_CONTENT = 'not_found_content',
+  KAKAO_CLIENT_ERROR = 'kakao_client_error',
+  NOT_ASSOCIATE_USER = 'not_asssociate_user',
 }
 
 export const ErrorMessage: { [key in ErrorCode]: string } = {
   [ErrorCode.UNAUTHORIZED]: 'Invalid Access Token',
   [ErrorCode.JWT_UNABLE_VERIFY]: 'Unable verify of jwt',
   [ErrorCode.NOT_FOUND_CONTENT]: 'Not found content',
+  [ErrorCode.KAKAO_CLIENT_ERROR]: 'Kakao client error',
+  [ErrorCode.NOT_ASSOCIATE_USER]: 'Not associate user',
 };
 
 export const ErrorStatus: { [key in ErrorCode]: HttpStatus } = {
   [ErrorCode.UNAUTHORIZED]: HttpStatus.UNAUTHORIZED,
   [ErrorCode.JWT_UNABLE_VERIFY]: HttpStatus.UNAUTHORIZED,
   [ErrorCode.NOT_FOUND_CONTENT]: HttpStatus.NOT_FOUND,
+  [ErrorCode.KAKAO_CLIENT_ERROR]: HttpStatus.INTERNAL_SERVER_ERROR,
+  [ErrorCode.NOT_ASSOCIATE_USER]: HttpStatus.UNAUTHORIZED,
 };
