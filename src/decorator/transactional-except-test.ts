b107c6d512b56bf44c05b9e0f6da84d79bb324f5
Junnnnnnnnnnn <wjsguswls71@naver.com>
2024-02-19 15:54:49 +0900

    test: TransactionalExceptTest Decorator

--- src/decorator/transactional-except-test.ts
+++ src/decorator/transactional-except-test.ts
@@ -2,6 +2,7 @@ import { applyDecorators } from '@nestjs/common';
 import { Transactional } from 'typeorm-transactional';
 
 export const TransactionalExceptTest = () => {
+  console.log('process.env.NODE_ENV', process.env.NODE_ENV);
   if (process.env.NODE_ENV !== 'test') return applyDecorators(Transactional());
   return applyDecorators();
 };
