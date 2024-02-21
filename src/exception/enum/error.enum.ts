3da8555811cb7f3ec364b51be08e17678299c92c
Junnnnnnnnnnn <wjsguswls71@naver.com>
2024-02-21 10:40:57 +0900

    [feat] 인증 된 유저 guard 개발

--- src/exception/enum/error.enum.ts
+++ src/exception/enum/error.enum.ts
@@ -6,6 +6,7 @@ export enum ErrorCode {
   NOT_FOUND_CONTENT = 'not_found_content',
   KAKAO_CLIENT_ERROR = 'kakao_client_error',
   NOT_ASSOCIATE_USER = 'not_associate_user',
+  NOT_NORMAL_USER = 'not_normal_user',
   ALREADY_AUTHENTICATED = 'already_authenticated',
   INTERNAL_SERVER_ERROR = 'internal_server_error',
 }
@@ -16,6 +17,7 @@ export const ErrorMessage: { [key in ErrorCode]: string } = {
   [ErrorCode.NOT_FOUND_CONTENT]: 'Not found content',
   [ErrorCode.KAKAO_CLIENT_ERROR]: 'Kakao client error',
   [ErrorCode.NOT_ASSOCIATE_USER]: 'Not associate user',
+  [ErrorCode.NOT_NORMAL_USER]: 'Not normal user',
   [ErrorCode.ALREADY_AUTHENTICATED]: 'Already authenticated',
   [ErrorCode.INTERNAL_SERVER_ERROR]: 'Internal server error',
 };
@@ -26,6 +28,7 @@ export const ErrorStatus: { [key in ErrorCode]: HttpStatus } = {
   [ErrorCode.NOT_FOUND_CONTENT]: HttpStatus.NOT_FOUND,
   [ErrorCode.KAKAO_CLIENT_ERROR]: HttpStatus.INTERNAL_SERVER_ERROR,
   [ErrorCode.NOT_ASSOCIATE_USER]: HttpStatus.UNAUTHORIZED,
+  [ErrorCode.NOT_NORMAL_USER]: HttpStatus.UNAUTHORIZED,
   [ErrorCode.ALREADY_AUTHENTICATED]: HttpStatus.BAD_REQUEST,
   [ErrorCode.INTERNAL_SERVER_ERROR]: HttpStatus.INTERNAL_SERVER_ERROR,
 };
---
+++ src/guard/auth.guard.spec.ts
@@ -0,0 +1,155 @@
+import { ExecutionContext } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { v4 as uuidV4 } from 'uuid';
+import sinon, { SinonStubbedInstance } from 'sinon';
+import { HttpError } from 'src/exception/service.error';
+import { AuthService } from 'src/module/auth/auth.service';
+import { UsersRepositoryService } from 'src/repository/service/users.repository.service';
+import { AuthGuard } from './auth.guard';
+import { createMock } from '@golevelup/ts-jest';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { ConfigService } from 'src/config/config.service';
+import {
+  TypeUsersGender,
+  TypeUsersSns,
+  TypeUsersStatus,
+} from 'src/repository/enum/users.repository.enum';
+import { UsersEntity } from 'src/repository/entity/users.entity';
+
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+  let authService: AuthService;
+  let usersRepositoryService: SinonStubbedInstance<UsersRepositoryService>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.register({
+          secret: ConfigService.getConfig().JWT.SECRET,
+        }),
+      ],
+    }).compile();
+
+    authService = new AuthService(module.get<JwtService>(JwtService));
+    usersRepositoryService = sinon.createStubInstance(UsersRepositoryService);
+    guard = new AuthGuard(authService, usersRepositoryService);
+  });
+
+  const createUsersEntity = (ctx: {
+    id?: number;
+    email?: string;
+    name?: string;
+    contact?: string;
+    experience?: number;
+    gender?: TypeUsersGender;
+    sns?: TypeUsersSns;
+    status?: TypeUsersStatus;
+    inputDate?: Date;
+    updateDate?: Date;
+    visitDate?: Date;
+    kakaoPk?: number;
+    comment?: string;
+  }): UsersEntity => {
+    const usersEntity = new UsersEntity();
+
+    usersEntity.id = ctx.id ?? 100;
+    usersEntity.email = ctx.email ?? `${uuidV4()}@kakao.com`;
+    usersEntity.name = ctx.email ?? uuidV4();
+    usersEntity.contact = ctx.contact ?? uuidV4();
+    usersEntity.experience = ctx.experience ?? 10;
+    usersEntity.gender = ctx.gender ?? TypeUsersGender.MAN;
+    usersEntity.sns = ctx.sns ?? TypeUsersSns.KAKAO;
+    usersEntity.status = ctx.status ?? TypeUsersStatus.NORMAL;
+    usersEntity.inputDate = ctx.inputDate ?? new Date();
+    usersEntity.updateDate = ctx.updateDate ?? new Date();
+    usersEntity.visitDate = ctx.visitDate ?? new Date();
+    usersEntity.kakaoPk = ctx.kakaoPk ?? 172957;
+    usersEntity.comment = ctx.comment ?? uuidV4();
+
+    return usersEntity;
+  };
+
+  describe('canActivate', () => {
+    it('should throw an error if JWT is missing', async () => {
+      const context = createMock<ExecutionContext>();
+      context.switchToHttp().getRequest.mockReturnValue({
+        headers: {},
+      });
+
+      // Act & Assert
+      await expect(guard.canActivate(context)).rejects.toThrow(HttpError);
+    });
+
+    it('should throw an error if user is not normal', async () => {
+      const context = createMock<ExecutionContext>();
+      const userEntity = createUsersEntity({
+        status: TypeUsersStatus.ASSOCIATE,
+      });
+
+      const jwt = await authService.getJwt(
+        { ...userEntity },
+        ConfigService.getConfig().JWT.LOGIN_EXPIRE_IN,
+      );
+
+      context.switchToHttp().getRequest.mockReturnValue({
+        headers: { authorization: `Bearer ${jwt}` },
+      });
+
+      usersRepositoryService.findUsersInfo.resolves(userEntity);
+
+      // Act & Assert
+      await expect(guard.canActivate(context)).rejects.toThrow(HttpError);
+    });
+
+    it.skip('should set userEntity and token in request', async () => {
+      const context = createMock<ExecutionContext>();
+      const userEntity = createUsersEntity({
+        status: TypeUsersStatus.NORMAL,
+      });
+
+      const jwt = await authService.getJwt(
+        { ...userEntity },
+        ConfigService.getConfig().JWT.LOGIN_EXPIRE_IN,
+      );
+
+      context.switchToHttp().getRequest.mockReturnValue({
+        headers: { authorization: `Bearer ${jwt}` },
+      });
+
+      usersRepositoryService.findUsersInfo.resolves(userEntity);
+
+      // Act
+      await guard.canActivate(context);
+
+      // Assert
+      expect(context.switchToHttp().getRequest()['userEntity']).toBe(
+        userEntity,
+      );
+
+      expect(context.switchToHttp().getRequest()['token']).toBe(jwt);
+    });
+
+    it.skip('should return true if all conditions are met', async () => {
+      const context = createMock<ExecutionContext>();
+      const userEntity = createUsersEntity({
+        status: TypeUsersStatus.NORMAL,
+      });
+
+      const jwt = await authService.getJwt(
+        { ...userEntity },
+        ConfigService.getConfig().JWT.LOGIN_EXPIRE_IN,
+      );
+
+      context.switchToHttp().getRequest.mockReturnValue({
+        headers: { authorization: `Bearer ${jwt}` },
+      });
+
+      usersRepositoryService.findUsersInfo.resolves(userEntity);
+      // Act
+      const result = await guard.canActivate(context);
+
+      // Assert
+      expect(result).toBe(true);
+    });
+  });
+});
